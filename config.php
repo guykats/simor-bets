2e34140974c410125171c5ebb1a56e4dd9970b0a
guykats <guy.kats@gmail.com>
2026-04-15 20:45:26 +0300

    Update config.php

--- config.php
+++ config.php
@@ -2,4 +2,4 @@
 define('DB_HOST', 'localhost');
 define('DB_NAME', 'u823311221_simor_bets');
 define('DB_USER', 'u823311221_simor_bets');
-define('DB_PASS', 'Simor_bets123!');
+define('DB_PASS', '');
